ca69b06f2d77c6e4bd8cb21e7d8fbb20ace332cc
Dan Phiffer <dan@phiffer.org>
2016-01-19 16:01:43 -0800

    🔧 Added VERSION to setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python
 
+# Remove .egg-info directory if it exists, to avoid dependency problems with
+# partially-installed packages (20160119/dphiffer)
+
+import os, sys
+from shutil import rmtree
+
+cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
+egg_info = cwd + "/mapzen.whosonfirst.mitie.egg-info"
+if os.path.exists(egg_info):
+    rmtree(egg_info)
+
 from setuptools import setup, find_packages
 
 packages = find_packages()
-desc = open("README.md").read(),
+desc = open("README.md").read()
+version = open("VERSION").read()
 
 setup(
     name='mapzen.whosonfirst.mitie',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.mitie'],
-    version='0.01',
+    version=version,
     description='Tools and libraries for working with Who\'s On First data and the MITIE tool-chain',
     author='Mapzen',
-    url='https://github.com/mapzen/py-mapzen-whosonfirst-mitie',
+    url='https://github.com/whosonfirst/py-mapzen-whosonfirst-mitie',
     install_requires=[
-        'mapzen.whosonfirst.placetypes',
-        'mapzen.whosonfirst.search',
+        'mapzen.whosonfirst.placetypes>=0.11',
+        'mapzen.whosonfirst.search>=0.12',
         ],
     dependency_links=[
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/tarball/master#egg=mapzen.whosonfirst.placetypes-0.11'
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-search/tarball/master#egg=mapzen.whosonfirst.search-0.12'
         ],
     packages=packages,
     scripts=[
